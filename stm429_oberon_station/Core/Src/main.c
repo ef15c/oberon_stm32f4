7146c29981bba7077291e42b8ca60ece3c0b0014
Christian Schoffit <sphpn@free.fr>
2024-04-26 11:10:32 +0200

    Portage en Oberon de l'initialisation du FMC et de la SDRAM
    Signed-off-by: Christian Schoffit <sphpn@free.fr>

--- stm429_oberon_station/Core/Src/main.c
+++ stm429_oberon_station/Core/Src/main.c
@@ -40,18 +40,14 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-SDRAM_HandleTypeDef hsdram1;
 
 /* USER CODE BEGIN PV */
-
-static FMC_SDRAM_CommandTypeDef Command;
 
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_FMC_Init(void);
 /* USER CODE BEGIN PFP */
 /* USER CODE END PFP */
 
@@ -101,7 +97,6 @@ SCnSCB->ACTLR |= SCnSCB_ACTLR_DISMCYCINT_Msk;
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_FMC_Init();
   /* USER CODE BEGIN 2 */
 
   BootLoadM4();
@@ -159,126 +154,6 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-}
-
-/* FMC initialization function */
-static void MX_FMC_Init(void)
-{
-
-  /* USER CODE BEGIN FMC_Init 0 */
-
-  /* USER CODE END FMC_Init 0 */
-
-  FMC_SDRAM_TimingTypeDef SdramTiming = {0};
-
-  /* USER CODE BEGIN FMC_Init 1 */
-
-  /* USER CODE END FMC_Init 1 */
-
-  /** Perform the SDRAM1 memory initialization sequence
-  */
-  hsdram1.Instance = FMC_SDRAM_DEVICE;
-  /* hsdram1.Init */
-  hsdram1.Init.SDBank = FMC_SDRAM_BANK2;
-  hsdram1.Init.ColumnBitsNumber = FMC_SDRAM_COLUMN_BITS_NUM_8;
-  hsdram1.Init.RowBitsNumber = FMC_SDRAM_ROW_BITS_NUM_12;
-  hsdram1.Init.MemoryDataWidth = FMC_SDRAM_MEM_BUS_WIDTH_16;
-  hsdram1.Init.InternalBankNumber = FMC_SDRAM_INTERN_BANKS_NUM_4;
-  hsdram1.Init.CASLatency = FMC_SDRAM_CAS_LATENCY_3;
-  hsdram1.Init.WriteProtection = FMC_SDRAM_WRITE_PROTECTION_DISABLE;
-  hsdram1.Init.SDClockPeriod = FMC_SDRAM_CLOCK_PERIOD_2;
-  hsdram1.Init.ReadBurst = FMC_SDRAM_RBURST_ENABLE;
-  hsdram1.Init.ReadPipeDelay = FMC_SDRAM_RPIPE_DELAY_1;
-  /* SdramTiming */
-  SdramTiming.LoadToActiveDelay = 2;
-  SdramTiming.ExitSelfRefreshDelay = 7;
-  SdramTiming.SelfRefreshTime = 4;
-  SdramTiming.RowCycleDelay = 7;
-  SdramTiming.WriteRecoveryTime = 3;
-  SdramTiming.RPDelay = 2;
-  SdramTiming.RCDDelay = 2;
-
-  if (HAL_SDRAM_Init(&hsdram1, &SdramTiming) != HAL_OK)
-  {
-    Error_Handler( );
-  }
-
-  /* USER CODE BEGIN FMC_Init 2 */
-
-/* Set the refresh rate counter */
-/* (15.62 us x Freq) - 20 */
-#define REFRESH_COUNT           ((uint32_t)1292)   /* SDRAM refresh counter */
-#define SDRAM_TIMEOUT           ((uint32_t)0xFFFF)
-
-/**
-  * @brief  FMC SDRAM Mode definition register defines
-  */
-#define SDRAM_MODEREG_BURST_LENGTH_1             ((uint16_t)0x0000)
-#define SDRAM_MODEREG_BURST_LENGTH_2             ((uint16_t)0x0001)
-#define SDRAM_MODEREG_BURST_LENGTH_4             ((uint16_t)0x0002)
-#define SDRAM_MODEREG_BURST_LENGTH_8             ((uint16_t)0x0004)
-#define SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL      ((uint16_t)0x0000)
-#define SDRAM_MODEREG_BURST_TYPE_INTERLEAVED     ((uint16_t)0x0008)
-#define SDRAM_MODEREG_CAS_LATENCY_2              ((uint16_t)0x0020)
-#define SDRAM_MODEREG_CAS_LATENCY_3              ((uint16_t)0x0030)
-#define SDRAM_MODEREG_OPERATING_MODE_STANDARD    ((uint16_t)0x0000)
-#define SDRAM_MODEREG_WRITEBURST_MODE_PROGRAMMED ((uint16_t)0x0000)
-#define SDRAM_MODEREG_WRITEBURST_MODE_SINGLE     ((uint16_t)0x0200)
-
-  __IO uint32_t tmpmrd =0;
-
-  /* SDRAM initialization */
-
-  /* Step 1:  Configure a clock configuration enable command */
-  Command.CommandMode             = FMC_SDRAM_CMD_CLK_ENABLE;
-  Command.CommandTarget           = FMC_SDRAM_CMD_TARGET_BANK2;
-  Command.AutoRefreshNumber       = 1;
-  Command.ModeRegisterDefinition  = 0;
-
-  /* Send the command */
-  HAL_SDRAM_SendCommand(&hsdram1, &Command, SDRAM_TIMEOUT);
-
-  /* Step 2: Insert 100 us minimum delay */
-  DWT_Delay_us(120);
-
-  /* Step 3: Configure a PALL (precharge all) command */
-  Command.CommandMode             = FMC_SDRAM_CMD_PALL;
-  Command.CommandTarget           = FMC_SDRAM_CMD_TARGET_BANK2;
-  Command.AutoRefreshNumber       = 1;
-  Command.ModeRegisterDefinition  = 0;
-
-  /* Send the command */
-  HAL_SDRAM_SendCommand(&hsdram1, &Command, SDRAM_TIMEOUT);
-
-  /* Step 4: Configure an Auto Refresh command */
-  Command.CommandMode             = FMC_SDRAM_CMD_AUTOREFRESH_MODE;
-  Command.CommandTarget           = FMC_SDRAM_CMD_TARGET_BANK2;
-  Command.AutoRefreshNumber       = 4;
-  Command.ModeRegisterDefinition  = 0;
-
-  /* Send the command */
-  HAL_SDRAM_SendCommand(&hsdram1, &Command, SDRAM_TIMEOUT);
-
-  /* Step 5: Program the external memory mode register */
-  tmpmrd = (uint32_t)SDRAM_MODEREG_BURST_LENGTH_1          |
-                     SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL   |
-                     SDRAM_MODEREG_CAS_LATENCY_3           |
-                     SDRAM_MODEREG_OPERATING_MODE_STANDARD |
-                     SDRAM_MODEREG_WRITEBURST_MODE_SINGLE;
-
-  Command.CommandMode             = FMC_SDRAM_CMD_LOAD_MODE;
-  Command.CommandTarget           = FMC_SDRAM_CMD_TARGET_BANK2;
-  Command.AutoRefreshNumber       = 1;
-  Command.ModeRegisterDefinition  = tmpmrd;
-
-  /* Send the command */
-  HAL_SDRAM_SendCommand(&hsdram1, &Command, SDRAM_TIMEOUT);
-
-  /* Step 6: Set the refresh rate counter */
-  /* Set the device refresh rate */
-  HAL_SDRAM_ProgramRefreshRate(&hsdram1, REFRESH_COUNT);
-
-  /* USER CODE END FMC_Init 2 */
 }
 
 /**
